56edc16f5d17ac104922c9987881c96423554567
yasmimrieg <yasmim_antonio@estudante.sesisenai.org.br>
2026-03-12 15:58:10 -0300

    Create atv.js

---
+++ 12-03/atv.js
@@ -0,0 +1,29 @@
+//1 Exercício de soma
+let num1= 5
+let num2= 2
+ console.log(num1+num2)
+
+ //2 Exercício de subtração
+ let num3= 3
+ let num4= 2
+ console.log(num3-num4)
+
+ //3 Exercício de multiplicação
+ let num5= 2
+ let num6= 2
+ console.log(num5*num6)
+
+ //4 Exercício de divisão
+ let num7= 4
+ let num8= 2
+ console.log(num7/num8)
+
+ //5 Exercício de modulo
+ let num9=12
+ let num10=5
+ console.log(num9%num10)
+
+ //6 Exercício de exponenciação
+ let num11= 2
+ let num12= 3
+ console.log(num11**num12)
